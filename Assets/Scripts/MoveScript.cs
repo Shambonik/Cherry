98530f0673afc4503ce8c51c37208dc64db8ff82
Shambonik <shambonik@gmail.com>
2020-07-14 17:33:17 +0300

    Moving and cloning

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+
+    private Vector3 run = new Vector3(0.2f,0,0);
+    private Vector3 position;
+    private RaycastHit hit;
+    private float ySpeed = 0;
+
+    /*
+     * 0 - d
+     * 1 - a
+     * 2 - space
+     * 3 - e
+     * 4 - left mouse button 
+     */
+
+    public Vector3 action(List<bool> buttons)
+    {
+        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1f)) ySpeed -= 0.05f;
+        else if(ySpeed<0) ySpeed = 0;
+        if ((buttons[2]) && (ySpeed == 0)) ySpeed = 0.7f; 
+        position = transform.position + new Vector3(0, ySpeed, 0);
+        if (buttons[0])
+        {
+            position = position + run;
+        }
+        else if (buttons[1])
+        {
+            position = position - run;
+        }
+        return position;
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private MoveScript moveScript;
+    private List<bool> buttons;
+    private Vector3 newPosition;
+    private List<List<bool>> history;
+    public GameObject copy;
+
+    
+    void Start()
+    {
+        moveScript = transform.GetComponent<MoveScript>();
+        newPosition = transform.position;
+        history = new List<List<bool>>();
+    }
+
+    /*
+    * 0 - d
+    * 1 - a
+    * 2 - space
+    * 3 - e
+    * 4 - left mouse button 
+    */
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.6f);
+        buttons = new List<bool> { Input.GetKey("d"), Input.GetKey("a"), Input.GetKey(KeyCode.Space)};
+        history.Add(buttons);
+        newPosition = moveScript.action(buttons);
+        if (Input.GetKeyDown("c"))
+        {
+            GameObject clone = Instantiate(copy);
+            clone.GetComponent<CloneScript>().setHistory(history);
+        }
+    }
+}
