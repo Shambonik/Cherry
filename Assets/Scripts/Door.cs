488ec6c674203443ddfdd5514c817e0480255d74
Totalexx <0702vitaly@gmail.com>
2020-07-17 00:25:57 +0500

    Wow, smart door it is real!

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Door : MonoBehaviour
 {
@@ -7,33 +8,37 @@ public class Door : MonoBehaviour
     private float shiftUp = 2f;
     private Vector2 closePosition;
     private Vector2 openPosition;
+    ContactFilter2D contactFilter = new ContactFilter2D();
+    List<RaycastHit2D> hits = new List<RaycastHit2D>();
 
-    private void Start() {
+    private void Start() 
+    {
         closePosition = new Vector2(transform.position.x, transform.position.y);
         openPosition = new Vector2(transform.position.x, transform.position.y + shiftUp);
     }
-    private void Update() {
+    private void Update() 
+    {
         if(isOpen)
         {
             transform.position = Vector2.Lerp(transform.position, openPosition, speed * Time.deltaTime);
         } else {
             transform.position = Vector2.Lerp(transform.position, closePosition, speed * Time.deltaTime);
         }
-        /*RaycastHit2D hit = Physics2D.Raycast(transform.position - transform.up - new Vector3(0 , 0.2f), Vector2.down, 2f);
-        Debug.DrawLine(transform.position - transform.up - new Vector3(0 , 0.2f), transform.position - transform.up - new Vector3(0 , 2.2f), Color.yellow);
-        if(isOpen == true && hit)
-        {   
-            
-            Debug.Log(hit.distance + " " + hit.collider);
-        }*/
     }
 
     public void Action()
     {
-        // if(isOpen == true && Physics2D.Raycast(transform.position, Vector2.down, shiftUp))
-        // {
-            
-        // }
-        isOpen = !isOpen;
+        int hits = Physics2D.Raycast(transform.position - transform.up - new Vector3(0 , 0.01f),
+                Vector2.down, contactFilter, this.hits, 1.5f);
+        if(isOpen)
+        {
+            if(hits == 0)
+            {
+                isOpen = false;
+            }
+        } else {
+            isOpen = true;
+        }
+        
     }
 }
