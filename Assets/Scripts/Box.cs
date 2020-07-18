529ac8b7afb8bd88cdc9926932665fa7b5c203e2
Totalexx <0702vitaly@gmail.com>
2020-07-18 13:41:48 +0500

    Add textures wall

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private bool isActivity = false;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(isActivity && Input.GetKeyUp("e"))
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player") isActivity = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "Player") isActivity = false;
+    }
+}
