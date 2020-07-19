08b2f0d743ff8766d3c960bc7d53176c83faf7a7
Totalexx <0702vitaly@gmail.com>
2020-07-20 00:38:40 +0500

    Add introduction level

---
+++ Assets/Scripts/GoLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoLevel : MonoBehaviour
+{
+    public int level;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Application.LoadLevel(level);
+    }
+}
